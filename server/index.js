fec5ad7991e03e1f1bead1138cbc7a60bae3d0bd
Igor Xavier <igorxavier.ixds@gmail.com>
2023-01-31 18:21:55 -0300

    fix: server deploy changes

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const server = require('http').Server(app);
 const cors = require('cors');
+
 app.use(cors());
-app.use((req, res, next) =>{
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use((req, res, next) =>{
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 const socketIO = require('socket.io')(server, {
   cors: {
